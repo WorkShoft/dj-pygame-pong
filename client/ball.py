18c3684d3d999191188b0cef4b7e89d1c3f61ac6
Mikel Losada <mikel.losada@gmail.com>
2020-07-22 19:11:55 +0200

    Create Ball class

---
+++ client/ball.py
@@ -0,0 +1,35 @@
+import pygame
+
+import constants
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, x=0, y=0, color=None, *args, **kwargs):
+        self.width = 10
+        self.height = 10
+
+        self.x = x
+        self.y = y
+
+        self.max_x = constants.SCREEN_WIDTH - self.width
+        self.max_y = constants.SCREEN_HEIGHT - self.height
+
+        self.color = color if color else (0, 0, 0)
+
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill(self.color)
+
+        self.rect = self.image.get_rect()
+
+        super(Ball, self).__init__(*args, **kwargs)
+
+    def update(self):
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def move(self, x=0, y=0):
+        if y < 0 and self.y > 0:
+            self.y += y
+
+        elif y > 0 and self.y < self.max_y:
+            self.y += y
