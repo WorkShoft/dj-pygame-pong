4032505dc5cdeec5e8f709a63ca8722088b35b5a
Mikel Losada <mikel.losada@gmail.com>
2020-07-23 10:35:23 +0200

    Write initial version of websockets client

---
+++ client/websockets_client.py
@@ -0,0 +1,10 @@
+import asyncio
+import websockets
+
+
+async def move(x, y):
+    uri = "ws://localhost:8000"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send({"x": x, "y": y, })
+        await websocket.recv()
+
