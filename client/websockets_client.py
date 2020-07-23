da396f3e68dc33a373ce0074d821b75017c9c1fc
Mikel Losada <mikel.losada@gmail.com>
2020-07-23 13:24:46 +0200

    Create client/resources folder and reformat websockets_client

--- client/websockets_client.py
+++ client/websockets_client.py
@@ -5,6 +5,7 @@ import websockets
 async def move(x, y):
     uri = "ws://localhost:8000"
     async with websockets.connect(uri) as websocket:
-        await websocket.send({"x": x, "y": y, })
+        await websocket.send(
+            {"x": x, "y": y,}
+        )
         await websocket.recv()
-
