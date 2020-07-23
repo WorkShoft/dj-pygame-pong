7540160eaa215f44203ac20f0a6907d818649e20
Mikel Losada <mikel.losada@gmail.com>
2020-07-23 13:24:09 +0200

    Install pong_app and channels on the Django project, and create ASGI app

--- server/server/settings.py
+++ server/server/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'pong_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ASGI_APPLICATION = 'server.routing.application'
