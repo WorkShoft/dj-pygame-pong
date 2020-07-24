a6848743c649f65bfeb064e8475f7df1692b5277
Mikel Losada <mikel.losada@gmail.com>
2020-07-24 17:29:06 +0200

    Cap FPS at 120

--- client/scene_manager.py
+++ client/scene_manager.py
@@ -1,12 +1,21 @@
+import json
+
+import pygame
+
+import websocket_client
+
+
 class SceneManager:
     def __init__(self, scenes):
         self.scenes = scenes
         self.current_scene = None
+        self.clock = pygame.time.Clock()
 
     def run(self):
         if self.current_scene:
             self.current_scene.update()
             self.current_scene.render()
+            self.clock.tick(120)
 
     def switch_scene(self, scene, **scene_data):
         if scene in self.scenes:
@@ -14,5 +23,3 @@ class SceneManager:
 
         if scene_data:
             self.current_scene.set_data(scene_data)
-
-            
