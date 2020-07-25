fedde9fa4361aa198c0b0b6abf866ee251e79130
Mikel Losada <mikel.losada@gmail.com>
2020-07-25 10:09:30 +0200

    Configure Redis channel layer

--- server/server/settings.py
+++ server/server/settings.py
@@ -116,3 +116,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 ASGI_APPLICATION = "server.routing.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {"hosts": [("127.0.0.1", 6379)],},
+    },
+}
