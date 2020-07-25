b6f3d59cb6c26d2cb8ef58b7a4fbc931d20e2a26
Mikel Losada <mikel.losada@gmail.com>
2020-07-25 10:10:51 +0200

    Propagate and stream game state using its own thread

--- server/pong_app/consumers.py
+++ server/pong_app/consumers.py
@@ -5,6 +5,8 @@ import constants
 import threading
 
 from .pong_controller import GameController, PaddleController, BallController
+
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
@@ -13,23 +15,33 @@ class PongConsumer(WebsocketConsumer):
     ball_controller = BallController()
 
     def connect(self):
+        self.game = self.scope["path"].strip("/")
+
+        async_to_sync(self.channel_layer.group_add)(self.game, self.channel_name)
+
         self.accept()
 
-        # self.state_thread = threading.Thread(target=self.stream_state)
-        # self.state_thread.daemon = True
-        # self.state_thread.start()
+        self.state_thread = threading.Thread(target=self.propagate_state)
+        self.state_thread.daemon = True
+        self.state_thread.start()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(self.game, self.channel_name)
 
     def receive(self, text_data):
         direction = json.loads(text_data).get("direction")
 
         PongConsumer.paddle_controller.move(direction)
-        PongConsumer.ball_controller.move()
 
-        self.send(text_data=json.dumps(GameController.state))
-
-    def stream_state(self):
+    def propagate_state(self):
         while True:
-            self.send(text_data=json.dumps(GameController.state))
+            PongConsumer.ball_controller.move()
+
+            async_to_sync(self.channel_layer.group_send)(
+                self.game, {"type": "stream_state", "state": GameController.state,}
+            )
+
+    def stream_state(self, event):
+        state = event["state"]
+
+        self.send(text_data=json.dumps(state))
