29a22ef0e9886a093f47afbee93bfc0d7de88796
Mikel Losada <mikel.losada@gmail.com>
2020-07-25 11:09:04 +0200

    Make Menu init less cluttered with load_sound and load_ui methods

--- client/menu.py
+++ client/menu.py
@@ -2,6 +2,7 @@ import pygame
 import time
 
 import constants
+import utils
 
 from base_scene import BaseScene
 
@@ -10,36 +11,22 @@ class Menu(BaseScene):
     def __init__(self, screen):
         self.screen = screen
 
-        self.logo = pygame.image.load("client/resources/pygame_powered.gif")
+        self.logo = pygame.image.load(utils.get_resource("pygame_powered.gif"))
         self.rect = self.logo.get_rect()
         self.x = constants.SCREEN_CENTER[0] - self.rect.width / 2
         self.y = constants.SCREEN_CENTER[1] - self.rect.height / 2
 
-        self.sound = pygame.mixer.Sound(
-            "client/resources/334261__projectsu012__coin-chime.wav"
-        )
-        self.typing_sound = pygame.mixer.Sound(
-            "client/resources/194799__jim-ph__keyboard5.wav"
-        )
+        self.game_name = ""
 
+        self.load_sound()
         self.sound.play(fade_ms=2000)
 
-        self.time = time.time()
-
-        self.title_font = pygame.font.Font(
-            "client/resources/street_cred/street_cred.ttf", 64
-        )
-        self.font = pygame.font.Font("client/resources/street_cred/street_cred.ttf", 36)
-
-        self.title = self.title_font.render("Pong", True, constants.BUTTON_COLOR)
-        self.plus_button = self.font.render("Enter game", True, constants.BUTTON_COLOR)
-        self.game_name = ""
-        self.type_prompt = self.font.render(
-            f"> {self.game_name}", True, constants.BUTTON_COLOR
-        )
+        self.load_ui()
 
         self.plus_button_rect = self.plus_button.get_rect()
 
+        self.time = time.time()
+        
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -62,6 +49,27 @@ class Menu(BaseScene):
         if keys[pygame.K_RETURN]:
             self.scene_manager.switch_scene("game", name=self.game_name)
 
+    def load_sound(self):
+        self.sound = pygame.mixer.Sound(
+            utils.get_resource("334261__projectsu012__coin-chime.wav")
+        )
+        self.typing_sound = pygame.mixer.Sound(
+            utils.get_resource("194799__jim-ph__keyboard5.wav")
+        )
+        
+    def load_ui(self):
+        self.title_font = pygame.font.Font(
+            "client/resources/street_cred/street_cred.ttf", 64
+        )
+        self.font = pygame.font.Font(utils.get_resource("street_cred/street_cred.ttf"), 36)
+
+        self.title = self.title_font.render("Pong", True, constants.BUTTON_COLOR)
+        self.plus_button = self.font.render("Enter game", True, constants.BUTTON_COLOR)
+
+        self.type_prompt = self.font.render(
+            f"> {self.game_name}", True, constants.BUTTON_COLOR
+        )
+        
     def render_ui(self):
         if time.time() - self.time < 3:
             self.screen.blit(self.logo, (self.x, self.y))
